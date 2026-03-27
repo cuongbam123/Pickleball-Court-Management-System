a7c0039e83379fd2a17a53cd8ffad663a34822a1
Sang080904 <sangquang0985275157@gmail.com>
2026-03-27 21:08:19 +0700

    feat/getid-update-delete

--- backend/src/routes/pricingRuleRoutes.js
+++ backend/src/routes/pricingRuleRoutes.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const pricingRuleController = require("../controllers/pricingRuleController");
 const {
   getPricingRulesQuerySchema,
+  pricingRuleIdParamSchema,
   createPricingRuleBodySchema,
+  updatePricingRuleBodySchema,
 } = require("../validations/pricingRuleValidation");
 
 const {
@@ -19,12 +21,35 @@ router.get(
   pricingRuleController.getPricingRules
 );
 
+router.get(
+  "/:id",
+  validate(pricingRuleIdParamSchema, "params"),
+  pricingRuleController.getPricingRuleDetail
+);
+
 router.post(
   "/",
   authenticate,
   authorizeRoles("admin"),
   validate(createPricingRuleBodySchema, "body"),
   pricingRuleController.createPricingRule
+);
+
+router.put(
+  "/:id",
+  authenticate,
+  authorizeRoles("admin"),
+  validate(pricingRuleIdParamSchema, "params"),
+  validate(updatePricingRuleBodySchema, "body"),
+  pricingRuleController.updatePricingRule
+);
+
+router.delete(
+  "/:id",
+  authenticate,
+  authorizeRoles("admin"),
+  validate(pricingRuleIdParamSchema, "params"),
+  pricingRuleController.deletePricingRule
 );
 
 module.exports = router;
