13323774a3b42af8c6473308cec85848118165e1
Sang080904 <sangquang0985275157@gmail.com>
2026-05-31 13:19:00 +0700

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const sharedMatchRoute = require("./routes/sharedMatchesRoute");
 const app = express();
 
 // middlewares
-const allowedOrigins = (process.env.CORS_ORIGIN || "http://localhost:5173")
+const allowedOrigins = (process.env.CORS_ORIGIN || "http://localhost:5173" || "https://pickleball-court-management-system-2.onrender.com")
   .split(",")
   .map((origin) => origin.trim())
   .filter(Boolean);
@@ -54,7 +54,7 @@ app.use("/api/v1/courts", courtRoutes)
 app.use("/api/v1/products", productRoutes);
 
 app.get('/', (req, res) => {
-    res.send(' API Pickleball đang hoạt động!');
+  res.send(' API Pickleball đang hoạt động!');
 });
 
 app.use((req, res, next) => {
